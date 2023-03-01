ef23d88826f2beea91342f1c10252ffea70d399f
Pontsho99 <pontshosibanda99@gmail.com>
2023-03-01 05:48:46 -0800

    5-main.c

--- 0x06-pointers_arrays_strings/5-main.c
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -3,16 +3,16 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	char str[] = "Look up!\n";
-	char *ptr;
-	
-	ptr = string_toupper(str);
-	printf("%s", ptr);
-	printf("%s", str);
-	return (0);
+    char str[] = "Look up!\n";
+    char *ptr;
+
+    ptr = string_toupper(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
 }
